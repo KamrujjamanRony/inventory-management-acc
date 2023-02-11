ada26fe72a2a4061b7dd105796cf5659ff5f1b0c
KamrujjamanRony <kamrujjaman0228@gmail.com>
2023-02-11 15:29:44 +0600

    product schema start

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 
 app.use(express.json());
 app.use(cors());
 
-
+// schema design
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, "Please provide a name for this product."],
+    trim: true,
+    unique: [true, "Name must be unique"],
+    minLength: [3, "Name must be at least 3 characters."]
+  }
+})
 
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
